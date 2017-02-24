2ce674d4bb5b15dea69964ba05ff3cd3b3de9272
dax <d.dax@email.com>
2017-02-24 12:53:33 +0100

    Add simple views for weather and unknown city

--- spec/weather_app_spec.rb
+++ spec/weather_app_spec.rb
@@ -3,19 +3,46 @@ require_relative 'spec_helper'
 class WeatherAppSpec < AppSpec
   let(:params) { Hash.new }
 
-  describe 'GET /random' do
-    let(:result) { get 'random', params }
+  describe 'GET' do
+    let(:result) { get path, params }
 
-    it 'handles the request' do
-      assert_equal 200, result.status
+    describe '/' do
+      let(:path) { '/' }
+
+      it 'handles the request' do
+        assert_equal 200, result.status
+      end
+    end
+
+    describe '/random' do
+      let(:path) { '/random' }
+
+      it 'handles the request' do
+        assert_equal 200, result.status
+      end
+    end
+
+    describe '/city' do
+      let(:path) { '/city' }
+
+      it 'handles the request' do
+        assert_equal 200, result.status
+      end
     end
   end
 
-  describe 'POST /city' do
-    let(:result) { post 'city', params }
+  describe 'POST' do
+    let(:params) do
+      { 'city': 'berlin, germany' }
+    end
+    let(:result) { post path, params }
 
-    it 'handles the request' do
-      assert_equal 200, result.status
+    describe '/city' do
+      let(:path) { '/city' }
+
+      it 'handles the request' do
+        assert_equal 200, result.status
+      end
     end
   end
 end
--- weather_app.rb
+++ weather_app.rb
@@ -1,11 +1,38 @@
 class WeatherApp < Sinatra::Application
-  get '/random' do
-    # display the weather at random coordinates
+  ## GET
+
+  get '/' do
+    haml :home
   end
 
+  get '/random' do
+    lat = rand(-90.0..90.0)
+    long = rand(-180.0..180.0)
+    forecast = Forecaster.new(coordinates: [lat, long])
+
+    serve_weather(forecast)
+  end
+
+  get '/city' do
+    haml :get_city
+  end
+
+  ## POST
+
   post '/city' do
-    # display the weather for the given city
-    # gracefully handle unknown cities
-    # optionally, cache the fetched weather data
+    city = params['city']
+    forecast = Forecaster.new(city: city)
+
+    serve_weather(forecast)
+  end
+
+  private
+
+  def serve_weather(forecast)
+    if forecast.unknown_location?
+      haml :unknown_location
+    else
+      haml :weather, locals: { forecast: forecast }
+    end
   end
 end
